af7be254458adf6ce0c890ab4f1b7f8db8d9b5ac
Ivan Matic <ivan.matic@cramick-it.com>
2020-10-06 11:46:06 +0200

    dif

--- azure_pipeline/fajl.cs
+++ azure_pipeline/fajl.cs
@@ -1,2 +1,5 @@
+git diff @~..@ --name-only --relative -- *.cs
 prvi
 drugi
+powershell -ExecutionPolicy Bypass -File .\find-change-and-increment-version.ps1 -ConfigFilePath "azure_pipeline/fajl.ver"
+set AZURE_PIPELINE=TRUE
